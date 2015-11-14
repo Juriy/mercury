d5e101c07320779ad79fe13d42552ae8349d559f
Juriy Bura <juriy.bura@gmail.com>
2015-11-14 21:22:40 +0800

    Fixed mark sending from client

--- client/js/aud.js
+++ client/js/aud.js
@@ -2,8 +2,8 @@
 
 var socket = io();
 $('button').click(function(){
-    socket.emit('mark', {"mark": $(this).attr("id")});
+    socket.emit('mark', {
+        mark: +$(this).attr('id').substr('5')
+    });
 });
-
-socket.on('marks', (msg) => {console.log(msg.total);});
 
