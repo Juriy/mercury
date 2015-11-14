052495e9f439f6c108de259a9eaa82102b19dfe9
Juriy Bura <juriy.bura@gmail.com>
2015-11-14 15:24:49 +0800

    Added listener tests

--- test/test.js
+++ test/test.js
@@ -5,8 +5,18 @@ let Classroom = require('../src/Classroom');
 
 describe('Classroom', () => {
     describe('#constructor', () => {
-        it('should construct object', () => {
+        it('should construct objects', () => {
             let cr = new Classroom();
+        });
+
+        it('should act as a listener', (done) => {
+            let cr = new Classroom();
+            cr.on('foo', (e)=> {
+                assert.equal(e, 1);
+                done();
+            });
+
+            cr.emit('foo', 1);
         });
     });
 });
