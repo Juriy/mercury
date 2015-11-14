79a3b19ffaa3a42f50c3ee0f1e6b4b517f8fb62f
Nick <nick.turunov@gmail.com>
2015-11-14 18:48:45 +0800

    port fix

--- main.js
+++ main.js
@@ -5,11 +5,13 @@ let app = express();
 let http = require('http').createServer(app);
 let io = require('socket.io')(http);
 
+var port = process.env.PORT || 3000;
+
 app.use(express.static(__dirname + '/client'));
 
 io.on('connection', onUserConnected);
 
-http.listen(3000, () => {console.log('listening on *:3000');});
+http.listen(port, () => {console.log('listening on *:3000');});
 
 function onUserConnected(socket) {
     socket.on('disconnect', () => console.log('user disconnected'));
